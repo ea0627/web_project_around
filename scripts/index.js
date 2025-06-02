f1a403dc9462909b6c35a82b688b90de3875a315
Eduardo Amaya <eduardo.amaya627@gmail.com>
2025-06-01 22:12:06 -0500

    se edita boton de cierre, boton de editar funcionando

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
-// JavaScript code goes here
+// Selección de elementos
+const popup = document.querySelector('.popup_type_edit-profile');
+const openButton = document.querySelector('.profile__edit-button');
+const closeButton = popup.querySelector('.popup__close-button');
+
+// Funciones
+function openPopup() {
+  popup.style.display = 'flex';
+}
+
+function closePopup() {
+  popup.style.display = 'none';
+}
+
+// Eventos
+openButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
